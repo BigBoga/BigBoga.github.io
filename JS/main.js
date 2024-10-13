abeb44171590ef3709d7af71015cbebb4eb91450
motor L <nloginova965@gmail.com>
2024-10-13 23:19:40 +0300

    adding my files to my repository

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+var myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello World";
+var ButtonNick = document.getElementById("NickChange")
+
+document.querySelector("html").oncontextmenu = function(){
+    alert("sex")
+}
+
+function setUserName(){
+    var name = prompt("Введите ваш ник")
+    if (!name){return}
+    localStorage.setItem("name", name)
+    myHeading.textContent = "Привет, " + name
+    console.log("Соси хуй")
+}
+
+if (!localStorage.getItem("name")){
+    setUserName()
+}
+else{
+    myHeading.textContent = "Привет, " + localStorage.getItem("name");
+}
+
+ButtonNick.onclick = function(){
+    setUserName();
+}
+
+C:\Users\nlogi\Documents\Programming\Sites\ttt.html
